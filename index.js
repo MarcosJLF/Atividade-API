7c26ea6cb8f945e7f3d18f54cc11f7b0af3a44cb
Marcos Junior <marcos_j_ferreira@estudante.sesisenai.org.br>
2024-09-23 17:19:21 -0300

    correcao

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import router from './Produtos/rotas.js'
 import routter from './clientes/rotas.js'
-<<<<<<< HEAD
 import row from './auth/rotas-auth.js'
+import rrouter from './login/rotas-login.js'
+import cookieParser from "cookie-parser"
 
 
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
 
 const swaggerDocument = YAML.load('./docs/documentacao.yaml')
-=======
-import rrouter from './login/rotas-login.js'
-import cookieParser from "cookie-parser"
->>>>>>> e437afbf4ae4f07b7ff074e3f09ceb8eaea990dd
 
 const app = express()
 
@@ -24,13 +21,10 @@ app.use('/produto',router)
 
 app.use('/cliente',routter)
 
-<<<<<<< HEAD
 app.use('/logar',row ) 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-=======
 app.use('/login', rrouter)
->>>>>>> e437afbf4ae4f07b7ff074e3f09ceb8eaea990dd
 
 const porta = 8080
 
